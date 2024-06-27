6113f5a96bf55e54827042781bb89ec9e0b847bb
becausefrancis <jimenez.francis92@gmail.com>
2024-06-27 13:59:43 -0500

    Committing to deploy.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const weapons  = {
-    type: ['sword', 'staff', 'bow', 'dagger'],
-    class: ['melee', 'ranged'],
-};
+
 
 /*---------------------------- Variables (state) ----------------------------*/
 
@@ -23,10 +20,6 @@ const render = () => {
     resultDisplayEl.textContent = `You chose ${playerChoice}.`
 };
 
-const addToInventory = () => {
-    inventory.push()
-};
-
 const startGame = () => {
     imageDisplayEl.src = 'https://images.unsplash.com/photo-1507707161256-bbcd7fe3359e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
     storyDisplayEl.textContent = 'You woke up in a cave. Right in front of you are four weapons. What are you choosing to start your journey outside?';
@@ -37,12 +30,9 @@ const startGame = () => {
     document.getElementById('startbutton').style.visibility = 'hidden';
 };
 
-/* function chooseWeapon(sword, staff, bow, dagger) {
-    let sword = document.querySelector('#A');
-    let staff = document.querySelector('#B');
-    let bow = document.querySelector('#C');
-    let dagger = document.querySelector('#D');
-    document.querySelectorAll('.btn').addEventListener('click', addToInventory);          // make sure to add a function to add the weapon to the list
+const choiceSword = () => {
+    inventory.push('Sword');
+    
 };
 
 /*----------------------------- Event Listeners -----------------------------*/
